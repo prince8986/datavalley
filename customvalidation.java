aa91d06305f1685c95236de35b72a9f40eebd4fd
AJAMEERA VENKATA BADHRI NAIK <95124518+prince8986@users.noreply.github.com>
2024-03-12 15:04:20 +0530

    Add files via upload

---
+++ customvalidation.java
@@ -0,0 +1,51 @@
+class CustomValidationException extends Exception {
+ 
+   public CustomValidationException(String message) {
+ 
+       super(message);
+ 
+   }
+
+}
+
+
+
+
+class NumberValidator {
+  
+  public static void validateNumber(int number) throws CustomValidationException {
+  
+      if (number < 0) {
+
+            throw new CustomValidationException("Number cannot be negative");
+      
+  }
+  
+ }
+
+}
+
+
+
+
+public class Exceptions1 {
+
+ public static void main(String[] args) {
+ 
+       int number = -5;
+   
+     try {
+ 
+           NumberValidator.validateNumber(number);
+  
+          System.out.println("Number is valid: " + number);
+ 
+       } catch (CustomValidationException e) {
+   
+         System.out.println("Error: " + e.getMessage());
+    
+    }
+   
+ }
+
+}
